159ec8592a28afb1a15651f78e4ea9f6f68d87dd
dickypagesdev <dicky.pages.dev@gmail.com>
2025-09-22 13:49:44 +0700

    Delete functions/api directory

--- functions/api/ambil-kelas.js
+++
@@ -1,6 +0,0 @@
-export async function onRequestGet({ env }) {
-  const rows = await env.DB.prepare("SELECT nama FROM classes ORDER BY nama").all();
-  return new Response(JSON.stringify(rows.results?.map(r=>r.nama)||[]), {
-    headers: { "Content-Type": "application/json" }
-  });
-}
